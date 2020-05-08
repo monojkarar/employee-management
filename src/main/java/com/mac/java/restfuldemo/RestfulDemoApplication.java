37299105f5da42b56dd59614059579fdaaec483e
Monoj Karar <monoj_karar@hotmail.com>
2020-05-08 07:25:27 +0530

    Initial project structure created.

---
+++ src/main/java/com/mac/java/restfuldemo/RestfulDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mac.java.restfuldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mac/java/restfuldemo/RestfulDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mac.java.restfuldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
